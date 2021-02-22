9d5ac8902d3428759ffe067491491ddf6b373b10
Eric Hurley <erichurley421@gmail.com>
2021-02-21 22:25:59 -0500

    Got timed colors working, able to store just one hour in localStorage right now

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,32 @@
 // Utilized JQuery to get the Day, Month, and day of month to display
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("dddd, MMMM Do"));
+console.log(currentDay);
+
+var actualTime = moment().hour();
+console.log(actualTime);
+
+function colorTime () {
+    for (var i = 9; i < 18; i++) {
+        if (i < actualTime) {
+            $("#" + i).addClass("past");
+        } else if (i === actualTime) {
+            $("#" + i).addClass("present");
+        } else {
+            $("#" + i).addClass("future");
+        }
+    }
+}
+colorTime();
+
+var textArea = $("#9");
+
+// function renderWork () {
+//     var textArea = localStorage.getItem("textArea", textArea);
+// }; 
+// renderWork();
+
+$(".saveBtn").on("click", function () {
+    var textArea = $("#9").val();
+    localStorage.setItem("textArea", textArea);
+})
